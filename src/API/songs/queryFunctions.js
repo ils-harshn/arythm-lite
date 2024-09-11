565ca6826c0a2261333cceecd24df2bd133b52ff
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-09-11 15:18:37 +0530

    filtered random sonf

--- src/API/songs/queryFunctions.js
+++ src/API/songs/queryFunctions.js
@@ -1,4 +1,5 @@
 import api from "..";
+import useFilterStore from "../../Store/filterStore";
 import ENDPOINTS from "../endpoints";
 
 export const getSongs = async (payload, pageParam) => {
@@ -19,13 +20,13 @@ export const getSongs = async (payload, pageParam) => {
   return response.data;
 };
 
-export const getRandomSong = async (payload, pageParam) => {
+export const getRandomSong = async () => {
+  const payload = useFilterStore.getState().filterData;
+
   const response = await api({
     method: "get",
     url: ENDPOINTS.GET_RANDOM_SONG,
     params: {
-      offset: pageParam,
-      limit: 10,
       original_name: payload?.original_name || "",
       album_title: payload?.album_title || "",
       album_code: payload?.album_code || "",
