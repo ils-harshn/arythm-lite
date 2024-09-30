c3bd4f9713410d30a035e4c3c48c58709472378d
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-09-30 18:38:27 +0530

    v4 done

--- src/Pages/MusicSelectionV4/index.js
+++ src/Pages/MusicSelectionV4/index.js
@@ -1,18 +1,20 @@
 import { motion } from "framer-motion";
 import DocumentMeta from "react-document-meta";
 import useSelectedSongStore from "../../Store/selectedSongStore";
-import { MusicPlayingTime, NoSongSelected, Thumbnail } from "../MusicSelection";
+import { NoSongSelected, Thumbnail } from "../MusicSelection";
 import { useEffect, useRef } from "react";
 import AudioMotionAnalyzer from "audiomotion-analyzer";
 import useMusicPlayerRefStore from "../../Store/musicPlayerRefStore";
+import { get_image_url } from "../../API/helpers";
 
-const AddVisualizer = () => {
+const AddVisualizer = ({ on }) => {
   const containerRef = useRef(null);
   const playerRef = useMusicPlayerRefStore((state) => state.music_player_ref);
   const audioMotionRef = useRef(null); // Track if it's already initialized
 
   useEffect(() => {
     const audioElement = playerRef?.current?.audio?.current;
+    const body = on.current;
 
     if (audioElement) {
       // Check if the audioMotionAnalyzer is already initialized
@@ -27,6 +29,17 @@ const AddVisualizer = () => {
           gradient: "rainbow",
           showScaleX: false,
           showPeaks: true,
+          onCanvasDraw: (instance) => {
+            const lowEnergy = instance.getEnergy("bass");
+            const midEnergy = instance.getEnergy("midrange");
+            const highEnergy = instance.getEnergy("treble");
+
+            const r = Math.round(lowEnergy * 255);
+            const g = Math.round(midEnergy * 255);
+            const b = Math.round(highEnergy * 255);
+
+            body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+          },
         });
       } else {
         // If audio is already connected, disconnect it first
@@ -43,25 +56,35 @@ const AddVisualizer = () => {
         audioMotionRef.current.destroy(); // Clean up the visualizer
         audioMotionRef.current = null; // Reset the ref
       }
+      body.style.backgroundColor = ``;
     };
-  }, [playerRef]);
+  }, [playerRef, on]);
 
-  return <div ref={containerRef} className="absolute w-full h-full"></div>;
+  return <div ref={containerRef} className="absolute w-full h-full z-0"></div>;
 };
 
 const Details = ({ song }) => {
+  const detailContainerRef = useRef(null);
+
   return (
     <motion.div
+      ref={detailContainerRef}
       className="w-full h-full flex justify-center items-center flex-col"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.8 }}
     >
+      <AddVisualizer on={detailContainerRef} />
+      <div
+        className="absolute top-0 left-0 w-full h-full opacity-10"
+      >
+        <img
+          className="block h-full w-full object-cover"
+          src={get_image_url(song.album.thumbnail)}
+          alt="backdp"
+        />
+      </div>
       <Thumbnail path={song.album.thumbnail} />
-      <div className="mt-1">{song.original_name}</div>
-      <div className="text-xs text-slate-300">{song.album.title}</div>
-      <MusicPlayingTime className="text-sm mt-2" />
-      <AddVisualizer />
     </motion.div>
   );
 };
